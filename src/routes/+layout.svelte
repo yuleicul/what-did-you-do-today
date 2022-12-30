18429896a5f85aee5c381623129a6df1ae455341
Yulei Chen <yulei_chen@epam.com>
2022-12-30 19:52:08 +0800

    Add sidebar

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,29 +1,29 @@
 <script>
-  let typeTree = [
-    {
-      type: 'Reactive work',
-      children: [
-        {
-          type: 'Tech skils',
-          children: []
-        },
-        {
-          type: 'English',
-          children: []
-        }
-      ]
-    },
-    {
-      type: 'Proactive work',
-      children: []
-    }
-  ];
+  import Icon from '@iconify/svelte';
+  export let data;
 </script>
 
 <div>
   <aside>
-    <!-- {#each timeTypes as type }
-			
-		{/each} -->
+    {#each data.typeTree as { name, icon, children } (name)}
+      <details>
+        <summary>
+          <Icon {icon} width="24" height="24" />
+          <span>{name}</span>
+        </summary>
+        <div>
+          {#each children as { name, icon } (name)}
+            <div>
+              <Icon {icon} width="24" height="24" />
+              <span>{name}</span>
+            </div>
+          {/each}
+        </div>
+      </details>
+    {/each}
   </aside>
+
+  <main>
+    <slot />
+  </main>
 </div>
