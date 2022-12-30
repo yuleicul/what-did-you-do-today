414ac540233974572b5beb19ed801df113869d68
Yulei Chen <yulei_chen@epam.com>
2022-12-30 19:13:03 +0800

    Prettier init

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,29 @@
+<script>
+  let typeTree = [
+    {
+      type: 'Reactive work',
+      children: [
+        {
+          type: 'Tech skils',
+          children: []
+        },
+        {
+          type: 'English',
+          children: []
+        }
+      ]
+    },
+    {
+      type: 'Proactive work',
+      children: []
+    }
+  ];
+</script>
+
+<div>
+  <aside>
+    <!-- {#each timeTypes as type }
+			
+		{/each} -->
+  </aside>
+</div>
